b7d5f2661f2f1b03463453e1d502a1e9f44bb08f
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-11-21 21:10:00 -0700

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,3 +1,5 @@
+### For the sake of the small additions made I will give the entire code per question.
+
 ###1)
 from psychopy import visual, monitors, event, core
 
@@ -88,4 +90,52 @@ win.close()
 ## With while loop we also have same level of percision at 0.999... seconds.
 
 ###3)
+
+### Added more images for countdown timer. Still percise in recording countdown.
+
+from psychopy import visual, monitors, event, core
+
+#define the monitor parameters
+mon = monitors.Monitor('myMonitor', width=31.4, distance=60)
+mon.setSizePix([1280,800])
+win = visual.Window(monitor=mon) #define a window
+
+import os
+#stuff you only have to define once at the top of your script
+main_dir = os.getcwd() 
+image_dir = os.path.join(main_dir,'images')
+
+fix_text = visual.TextStim(win,text='+')
+my_image = visual.ImageStim(win)
+
+stims = ['face01.jpg','face02.jpg','face03.jpg', 'face04.jpg', 'face05.jpg', 'face06.jpg', 'face07.jpg'] #create a list if images to show  
+nTrials= len(stims)
+waitTimer = core.Clock() #meta timer for stimuli
+stimTimer = core.CountdownTimer() # added stim timer to time stimuli
+
+for trial in range(nTrials):
+    
+    my_image.image = os.path.join(image_dir,stims[trial])
+    
+    fix_text.draw()
+    win.flip()
+    core.wait(.5)
+    
+    stimTimer.reset()
+    stimTimer.add(1)
+    imgStartTime = waitTimer.getTime()
+    
+    while stimTimer.getTime() > 0: #added while loop
+        my_image.draw()
+        win.flip()
+    imgEndTime = waitTimer.getTime()
+    
+    fix_text.draw()
+    win.flip()
+    core.wait(.5)
+    
+    print("Image Duration (s) was {} seconds".format(imgEndTime - imgStartTime))
+win.close()
+
+###4)
 
